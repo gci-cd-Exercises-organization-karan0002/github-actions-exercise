b60fa20fd4ccb3f97dfc6cc5931ca57429a9566e
Karan02 <krnptl1234@gmail.com>
2026-05-29 14:26:40 +0530

    ...

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -2,6 +2,7 @@ const core = require('@actions/core')
 const github = require('@actions/github')
 const exec = require('@actions/exec')
 
+// Note Custom Javascript actions must include all the code and all the dependencies, so node_modules are also pushed
 async function run() {
   core.notice('Hello from my custom Javascript Action!')
 }
